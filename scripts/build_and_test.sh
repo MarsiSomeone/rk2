1a073a5cd4fc808d7de63ee7f0c8912fced93458
MarsiSomeone <sirlovaolga@gmail.com>
2026-05-26 05:09:25 +0300

    x

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-mkdir -p build
+rm -rf build
+mkdir build
 cd build
-cmake ..
+cmake .. -DBUILD_TESTS=ON
 cmake --build .
 ctest --output-on-failure
 cmake --build . --target package
 mkdir -p ../Release
-cp *.deb ../Release/
+cp Release/*.deb ../Release/ 
